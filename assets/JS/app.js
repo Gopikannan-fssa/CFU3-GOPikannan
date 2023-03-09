d045445d4e482d1ad40e0aa54652f08412429d26
Gopikannan-fssa <118425968+Gopikannan-fssa@users.noreply.github.com>
2023-03-09 19:17:36 +0530

    initial commit

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -10,17 +10,15 @@ let generatedpwd = "";
 let form = document.getElementById("form");
 
 form.addEventListener("submit", function (event) {
-  event.preventDefault();
+  
   if (document.getElementById("uppercase")) {
     for (let i = 1; i <= pwdlength; i++) {
       let char = Math.floor(Math.random() * upperAlpbh.length);
       generatedpwd += upperAlpbh.substring(char, char + 1);
     }
     document.getElementById("password__result").value = generatedpwd;
-    return;
-  } else {
-    return;
-  }
+    
+  } 
 });
 
 // console.log(generatedpwd);
