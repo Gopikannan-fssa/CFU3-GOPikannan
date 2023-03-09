c55f9698f8de6cbad5768b23496fb7c6376a07ca
Gopikannan-fssa <118425968+Gopikannan-fssa@users.noreply.github.com>
2023-03-09 18:34:29 +0530

    initial commit

---
+++ assets/JS/app.js
@@ -0,0 +1,35 @@
+/*get the number of characters*/
+let alpbh = "abcdefghijklmnopqrstuvwxyz";
+let upperAlpbh = alpbh.toUpperCase();
+let lowerAlpbh = alpbh.toLowerCase();
+let pwdlength = 10;
+let generatedpwd = "";
+
+// function generate() {
+
+let form = document.getElementById("form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if (document.getElementById("uppercase")) {
+    for (let i = 1; i <= pwdlength; i++) {
+      let char = Math.floor(Math.random() * upperAlpbh.length);
+      generatedpwd += upperAlpbh.substring(char, char + 1);
+    }
+    document.getElementById("password__result").value = generatedpwd;
+    return;
+  } else {
+    return;
+  }
+});
+
+// console.log(generatedpwd);
+
+function charLength() {
+  let charLength = document.getElementById("length").value;
+  //   console.log(charLength);
+  document.getElementById("result").innerHTML = charLength;
+  return charLength;
+}
+
+document.getElementById("length").addEventListener("change", charLength);
